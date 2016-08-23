db14350ba81c51cb7966bbdb640409b920eaad7b
Renan Silva <renan.tssilva@gmail.com>
2016-08-23 18:50:44 -0300

    save progress in creating piles

--- ep2.c
+++ ep2.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ** criaTabuleiro(int m, int n) {
+
+typedef struct {
+    int * v;
+    int topo;
+} pilha;
+
+pilha * criaPilha(int n) {
+    pilha * p;
+    p = malloc(sizeof(pilha));
+    (*p).v = calloc(n, sizeof(int));
+    (*p).topo = 0;
+    return p;
+}
+
+/*void empilha(pilha * p, int x) {
+    (*p).v[(*p).topo++]
+}*/
+
+int ** criaMatriz(int m, int n) {
     
     int i, j;
     int ** tab;
@@ -11,7 +29,7 @@ int ** criaTabuleiro(int m, int n) {
 
     return tab;
 }
-void destroiTabuleiro(int ** tab, int m, int n) {
+void destroiMatriz(int ** tab, int m, int n) {
     int i;
     for (i = 0; i < m; i++)
         free(tab[i]);
@@ -19,7 +37,8 @@ void destroiTabuleiro(int ** tab, int m, int n) {
     free(tab);
 }
 
-void imprimeTabuleiro(int ** tab, int m, int n) {
+
+void imprimeMatriz(int ** tab, int m, int n) {
     int i, j;
 
     for (i = 0; i < m; i++) {
@@ -27,13 +46,29 @@ void imprimeTabuleiro(int ** tab, int m, int n) {
             printf("%d ", tab[i][j]);
         printf("\n");
     }
+}
+
+/*essa função será apagada antes de entregar o ep,
+é apenas para testes. Recebe a matriz, m, n e a distancia entre os 1 e o vertice*/
+void setJogo(int ** tab, int m, int dist) {
+    int i, j;
+    for(i = 0; i < m; i++)
+        for (j = dist; j < m - dist; j++) {
+            tab[i][j] = 1;
+            tab[j][i] = 1;
+        }
+    tab[(m-1)/2][(m-1)/2] = -1;
+}
+
+void jogo(int n) {
 
 }
 int main(int argc, char const *argv[])
 {
     int ** tab;
-    tab = criaTabuleiro(10, 10);
-    imprimeTabuleiro(tab, 10, 10);
-    destroiTabuleiro(tab, 10, 10);
+    tab = criaMatriz(7, 7);
+    setJogo(tab, 7, 2);
+    imprimeMatriz(tab, 7, 7);
+    destroiMatriz(tab, 7, 7);
     return 0;
 }
