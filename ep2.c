52c108c58fec835b5a140ffe372e0c1a25800dba
Renan Silva <renan.tssilva@gmail.com>
2016-08-23 18:03:55 -0300

    add tabuleiro functions

--- ep2.c
+++ ep2.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int ** criaTabuleiro(int m, int n) {
+    
+    int i, j;
+    int ** tab;
+    tab = malloc(m * sizeof(int *));
+    for (i = 0; i < m; i++)
+        tab[i] = calloc(n, sizeof(int));
+
+    return tab;
+}
+void destroiTabuleiro(int ** tab, int m, int n) {
+    int i;
+    for (i = 0; i < m; i++)
+        free(tab[i]);
+
+    free(tab);
+}
+
+void imprimeTabuleiro(int ** tab, int m, int n) {
+    int i, j;
+
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++)
+            printf("%d ", tab[i][j]);
+        printf("\n");
+    }
+
+}
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int ** tab;
+    tab = criaTabuleiro(10, 10);
+    imprimeTabuleiro(tab, 10, 10);
+    destroiTabuleiro(tab, 10, 10);
     return 0;
 }
